46ea14ce78c59191e06feaccdacd2b457e289734
sagar kale <kalesagar122@gmail.com>
2017-02-24 21:47:52 -0800

    Intial

---
+++ blog/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .models import Post
+# Create your views here.
+
+def post_list(request):
+ post=Post.objects.order_by('published_date')
+ return render(request,'blog/post_list.html',{'posts':post})
